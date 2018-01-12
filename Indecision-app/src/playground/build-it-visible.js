9fe6e92376923aa8875fcb000e07012e74248dc4
Jesse Hanner <j-money@Jesses-MacBook-Pro.local>
2018-01-12 14:29:29 -0600

    Changed build-it-visible.js entirely to using state and components

--- Indecision-app/src/playground/build-it-visible.js
+++ Indecision-app/src/playground/build-it-visible.js
@@ -1,30 +1,30 @@
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    };
+  }
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility,
+      };
+    });
+  }
+  render() {
+    return (
+        <div>
+          <h1>Visibility Toggle</h1>
+          <button onClick={this.handleToggleVisibility}>
+            {this.state.visibility ? 'Hide details': 'Show details'}
+          </button>
+            {this.state.visibility ? <p>Nothing here you fool!.</p>: <p>It's a secret yo. Click to see.</p>}
+        </div>
+        );
+  }
+}
 
 
-
-// const app = {
-//   title: "Visibility Toggle",
-//   text: "There was no secret you fool!"
-// }
-// let text = false;
-//
-// const toggleVisibility = () => {
-//   text = !text;
-//   render();
-// };
-//
-// const appRoot = document.getElementById('app');
-//
-// const render = () => {
-//   const template = (
-//   <div>
-//     <h1>{app.title}</h1>
-//     <button onClick={toggleVisibility}>
-//       {text ? 'Hide details': 'Show details'}
-//     </button>
-//       {text ? <p>{app.text}</p>: <p>It's a secret yo. Click to see.</p>}
-//   </div>
-//   );
-//   ReactDOM.render(template, appRoot);
-// };
-//
-// render();
+ReactDOM.render(<VisibilityToggle/>, document.getElementById('app'));
