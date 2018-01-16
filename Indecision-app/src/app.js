20f54d7055c0f5aa60787d0f5112e9cce371e129
Jesse Hanner <j-money@Jesses-MacBook-Pro.local>
2018-01-16 15:00:43 -0600

    just checking push

--- Indecision-app/src/app.js
+++ Indecision-app/src/app.js
@@ -6,27 +6,3 @@ import IndecisionApp from './components/IndecisionApp';
 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
-
-// class OldSyntax {
-//   constructor() {
-//     this.name = 'Mike';
-//   }
-//   getGreeting() {
-//     return `HI, my name is ${this.name}`;
-//   }
-// }
-//
-// const oldSyntax = new OldSyntax();
-// const getGreeting = oldSyntax.getGreeting;
-// console.log(getGreeting());
-
-// class NewSyntax {
-//   name = "Jesse";
-//   getGreeting = () => {
-//     return `HI, my name is ${this.name}`;
-//   }
-// }
-//
-// const newSyntax = new NewSyntax();
-// const newGetGreeting = newSyntax.getGreeting;
-// console.log(newGetGreeting());
--- Indecision-app/src/components/IndecisionApp.js
+++ Indecision-app/src/components/IndecisionApp.js
@@ -14,76 +14,69 @@ export default class IndecisionApp extends React.Component {
     }
     else if(this.state.options.indexOf(option) > -1) {
       return 'Already in option list.';
-    }
+    };
 
     this.setState((prevState) => ({options: prevState.options.concat(option)}));
-  }
+  };
 
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length); //choose a random number depending on options.length
     const option = this.state.options[randomNum]; //use randomNum to determine which array element to access
     alert(option);
-  }
+  };
 
   handleDeleteOptions = () => {
     this.setState(() => ({options: [] }));
-  }
+  };
 
   handleDeleteOption = (optionToRemove) => {
     this.setState((prevState) => ({
       options: prevState.options.filter((option) => optionToRemove !== option)
     }));
-  }
+  };
 
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption =  this.handleDeleteOption.bind(this);
-}
-componentDidMount() { //use for fetching data
-  try {
-    const json = localStorage.getItem('options');
-    const options = JSON.parse(json);
+  render = () => {
+      const subTitle = "Put your life in my hands muhahaha";
 
-    if(options) {
-      this.setState(() => ({options: options})); //({options}) is the same
+      return (
+        <div>
+          <Header subTitle={subTitle} />
+          {/*these components save code and can be easily reused. They are almost like class templates*/}
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick} //adding props
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+        </div>
+      );
+
+    };
+    componentDidMount = () => { //use for fetching data
+      try {
+        const json = localStorage.getItem('options');
+        const options = JSON.parse(json);
+
+        if(options) {
+          this.setState(() => ({options: options})); //({options}) is the same
+        }
+      } catch(e) {
+
+      }
     }
-  } catch(e) {
-
-  }
-}
-componentDidUpdate(prevProps, prevState) { //use for saving data
-  if(prevState.options.length !== this.state.options.length) {
-    const json = JSON.stringify(this.state.options);
-    localStorage.setItem('options', json);
-  }
-}
-componentWillUnmount() {
-  console.log("component will unmount")
-}
-
-render() {
-    const subTitle = "Put your life in my hands muhahaha";
-
-    return (
-      <div>
-        <Header subTitle={subTitle} />
-        {/*these components save code and can be easily reused. They are almost like class templates*/}
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick} //adding props
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
-      </div>
-    );
-  }
-}
+    componentDidUpdate = (prevProps, prevState) => { //use for saving data
+      if(prevState.options.length !== this.state.options.length) {
+        const json = JSON.stringify(this.state.options);
+        localStorage.setItem('options', json);
+      }
+    }
+    componentWillUnmount = () => {
+        console.log("component will unmount")
+      }
+    }
