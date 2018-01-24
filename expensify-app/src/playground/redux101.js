76cef732e81c7eb90a0fc27052ef7b93eae35af8
Jesse Hanner <j-money@Jesses-MacBook-Pro.local>
2018-01-24 13:11:45 -0600

    Adding Redux to React project

--- expensify-app/src/playground/redux101.js
+++ expensify-app/src/playground/redux101.js
@@ -1,17 +1,33 @@
 import {createStore} from 'redux';
 
+const incrementCount = ({incrementBy = 1} = {}) => ({
+  type: 'INCREMENT',
+  incrementBy: incrementBy
+});
 
-const store = createStore((state = {count: 0, text: 'yo'}, action) => {
+const decrementCount = ({decrementBy = 1} = {}) => ({
+  type: 'DECREMENT',
+  decrementBy: decrementBy
+});
+
+const setCount = ({count} = {}) => ({
+  type: 'SET',
+  count: count
+});
+
+const resetCount = () => ({
+  type: 'RESET',
+});
+
+const countReducer = (state = {count: 0}, action) => {
   switch (action.type) {
     case 'INCREMENT':
-    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy: 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
       case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy: 1;
         return {
-          count: state.count - decrementBy
+          count: state.count - action.decrementBy
         };
       case 'RESET':
         return {
@@ -24,31 +40,22 @@ const store = createStore((state = {count: 0, text: 'yo'}, action) => {
       default:
       return state;
     }
-});
+};
+
+const store = createStore(countReducer);
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+store.dispatch(incrementCount({incrementBy: 5}));
 
-store.dispatch({
-  type: 'DECREMENT'
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 15
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: 'RESET'
-});
+store.dispatch(decrementCount({decrementBy: 10}));
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+store.dispatch(setCount({count: 101}));
+
+store.dispatch(resetCount());
