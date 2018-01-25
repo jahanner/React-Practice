12a3d9fe710ad39744c6d5bbeacd32e39940d330
Jesse Hanner <j-money@Jesses-MacBook-Pro.local>
2018-01-25 16:44:55 -0600

    added calendar picker

--- expensify-app/src/components/AddExpense.js
+++ expensify-app/src/components/AddExpense.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
 
 const HomePage = () => (
   <div>
-    Expensify add expense page
+    <h1>Add expense</h1>
+    <ExpenseForm />
   </div>
 );
 
--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-const linkTo = (route, text) => { {/*quick function to set up links*/}
-    if(route === '/'){
-      return <p><NavLink to={route} activeClassName='is-active' exact={true}>{text}</NavLink></p>
+const linkTo = (route, text) => {
+  {
+    /*quick function to set up links*/
   }
-  else {
-    return <p><NavLink to={route} activeClassName='is-active'>{text}</NavLink></p>
-  };
-}
+  if (route === "/") {
+    return (
+      <p>
+        <NavLink to={route} activeClassName="is-active" exact={true}>
+          {text}
+        </NavLink>
+      </p>
+    );
+  } else {
+    return (
+      <p>
+        <NavLink to={route} activeClassName="is-active">
+          {text}
+        </NavLink>
+      </p>
+    );
+  }
+};
 
 const Header = () => (
   <header>
     <h1>Expensify</h1>
-      {linkTo('/help', 'Help Page')}
-      {linkTo('/', 'Home Page')}
-      {linkTo('/create', 'AddExpense Page')}
-      {linkTo('/edit', 'Expense')}
+    {linkTo("/help", "Help")}
+    {linkTo("/", "Dashboard")}
+    {linkTo("/create", "Create Expense")}
+    {linkTo("/edit", "Expense")}
   </header>
-)
+);
 
 export default Header;
